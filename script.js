c2ba0fd73e2aca68a152f2dc27399ac36fa820a1
Architgarg603 <architgarg603@gmail.com>
2022-06-14 20:27:36 +0530

    fd

---
+++ script.js
@@ -0,0 +1,28 @@
+let btn = document.querySelector("button")
+let text = document.querySelector("h3")
+let arr = [
+    " Never have I ever went more than a day without brushing my teeth.",
+    " Never have I ever had crush on my friend's partner.",
+    " Never have I ever been ony any dating app.",
+    " Never have I ever been to the opposite gender's washroom.",
+    "Never have I ever tried to make an ex jealous",
+    " Never have I ever lied during this game!",
+    " Never have I ever made out in public.",
+    " Never have I ever cheated or was cheated in a relationship.",
+    " Never have I ever flirted with a person who is in a comitted relationship.",
+    " Never have I ever fallen in love at first sight.",
+    " Never have I ever stolen something.",
+    " Never have I ever caught cheating on a test.",
+    " Never have I ever lied about my age to someone.",
+    " Never have I ever had someone slap me across the face.",
+    " Never have I ever brushed with someone else's toothbruth.",
+]
+
+let idx = 0;
+
+btn.addEventListener("click", () => {
+    if (idx >= arr.length) idx = 0;
+    text.innerHTML = arr[idx++];
+    btn.innerHTML = "Next"
+})
+
